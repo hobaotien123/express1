4a3667b398fcf166838876ba6e6b5feb561c0a4d
Your Name <you@example.com>
2020-05-11 19:13:53 +0700

    'new-users-list-tast2-express'

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+// dùng pug 
+ 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+
+
 app.get('/',function(req,res){
-	res.send('Hello Tiến');
+	res.render('index.pug');
 });
 
-app.get('/todos',function(req,res){
-	res.send('<ul><li>Đi chợ</li><li>Đi chợ</li><li>Đi chợ</li><li>Đi chợ</li></ul>');
+app.get('/users',function(req,res){
+	res.render('users/users.pug',{
+		users: [
+			{ id : 1, name : 'Thinh'},
+			{ id : 2, name : 'Tánh'},
+			{ id : 3, name : 'Phương'},
+			{ id : 4, name : 'Linh'}
+		]
+	});
 });
 
 app.listen(port,function(){
