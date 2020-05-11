5637ce20f81413999b47dc678ffe785c323aa7df
Your Name <you@example.com>
2020-05-11 22:16:50 +0700

    'Express-3'

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ var port = 3000;
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-
+var users = [
+			{ id : 1, name : 'Thinh'},
+			{ id : 2, name : 'Tánh'},
+			{ id : 3, name : 'Phương'},
+			{ id : 4, name : 'Linh'},
+			{ id : 5, name : 'Nấu'},
+			{ id : 6, name : 'Đi'},
+			{ id : 7, name : 'Làm'}
+		];
 
 app.get('/',function(req,res){
 	res.render('index.pug');
@@ -15,14 +23,19 @@ app.get('/',function(req,res){
 
 app.get('/users',function(req,res){
 	res.render('users/users.pug',{
-		users: [
-			{ id : 1, name : 'Thinh'},
-			{ id : 2, name : 'Tánh'},
-			{ id : 3, name : 'Phương'},
-			{ id : 4, name : 'Linh'}
-		]
+		users: users
 	});
 });
+
+app.get('/users/search',function(req,res){
+	var q = req.query.q;
+	var matchedUsers = users.filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/users.pug',{
+		users : matchedUsers
+	})
+})
 
 app.listen(port,function(){
 	console.log('Sever is start port');
