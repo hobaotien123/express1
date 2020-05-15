103bd5e4b572ce894618b31be8edfd63a7225eb9
Your Name <you@example.com>
2020-05-15 11:11:10 +0700

    'Express-4-method-POST'

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var port = 3000;
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+//// dùng req.body lấy dữ liệu từ client người dùng gửi lên server
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 var users = [
 			{ id : 1, name : 'Thinh'},
 			{ id : 2, name : 'Tánh'},
@@ -35,6 +41,15 @@ app.get('/users/search',function(req,res){
 	res.render('users/users.pug',{
 		users : matchedUsers
 	})
+})
+
+app.get('/users/create',function(req,res){
+	res.render('users/create.pug');
+})
+
+app.post('/users/create', function(req,res){
+	users.push(req.body);
+	res.redirect('/users');
 })
 
 app.listen(port,function(){
